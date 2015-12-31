abbcc05fffbb5c0b0aa07b1650ef0ee109938e17
Ryohei Ueda <garaemon@gmail.com>
2015-12-31 21:28:18 +0900

    Update message

--- src/hook/ros.cpp
+++ src/hook/ros.cpp
@@ -16,13 +16,14 @@ namespace ros
 
   void ros_start_glc_callback(const std_msgs::Empty::ConstPtr& empty_msg)
   {
-    ROS_INFO("ros_start_glc");
+    ROS_INFO("[%s] ros_start_glc", ros::this_node::getName().c_str());
     start_capture();
   }
 
   void ros_stop_glc_callback(const std_msgs::Empty::ConstPtr& empty_msg)
   {
-    ROS_INFO("ros_stop_glc");
+    ros::NodeHandle nh;
+    ROS_INFO("[%s] ros_stop_glc", ros::this_node::getName().c_str());
     start_capture();
   }
   
@@ -31,9 +32,8 @@ namespace ros
     
     typedef void (*fptr)(int& argc, char** argv, const std::string& name, uint32_t options);
     void (*o_init) (int& argc, char** argv, const std::string& name, uint32_t options);
-    std::cerr << "my init" << std::endl;
-    //o_init = reinterpret_cast<fptr>(dlsym(RTLD_NEXT, "ros::init"));
-    o_init = reinterpret_cast<fptr>(dlsym(RTLD_NEXT, "_ZN3ros4initERiPPcRKSsj"));
+    ROS_WARN("ros::init is overtaken by glc-capture");
+    o_init = reinterpret_cast<fptr>(dlsym(RTLD_NEXT, "_ZN3ros4initERiPPcRKSsj")); // mangled ros::init
     if (o_init) {
       o_init(argc, argv, name, options);
       ros::NodeHandle n;
